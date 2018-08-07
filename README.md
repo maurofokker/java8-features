da7c1175a6b12a2346113694075571a868376db2
Mauricio <mgaldamesc.azurian@bancochile.cl>
2018-08-07 11:38:15 -0400

    demo for Consumer functional interface

--- README.md
+++ README.md
@@ -37,3 +37,41 @@
         void run();
     }
   ``` 
+
+## Functional Interfaces
+
+* Exists since java 1.0
+* Known as Single Abstract Method, is an interface that has exactly one abstract method
+* In java 8 use the optional annotation `@FunctionalInterface`
+* Principals functional interfaces in java 8
+  * Consumer
+  * Predicate
+  * Function
+  * Supplier
+* Presents in the `java.util.function` package
+
+### Consumer Functional Interface
+
+* Consumer can be used in all contexts where an object needs to be consumed.
+  * Object is taken as input and some operations are performed on the object without returning any result
+  * `(T t) -> {}`
+* Since it is a functional interface, it can be used as the assignment target for a lambda expression or a method reference
+* It has one abstract method `void accept(T t)` that accepts one single input argument and return nothing
+* It has a default method `default Consumer<T> andThen(Consumer<? super T> after)` (From java 8 interfaces can have `default` methods) 
+* It is a function that is representing side effects
+* Iterable interface has a default `forEach` method that accepts a Consumer as argument
+  ```java
+    default void forEach(Consumer<? super T> action) {
+      Objects.requireNonNull(action);
+        for (T t : this) {
+          action.accept(t);
+        }  
+    }
+  ```
+  
+  ```java
+    Consumer<String> consumer = s -> System.out.println(s.toUpperCase());
+    List<String> names = Arrays.asList("John", "Doe");
+    names.forEach(consumer);
+    // output: JOHN \n DOE
+  ```
--- src/main/java/com/maurofokker/java8/functionalinterfaces/ConsumerDemo.java
+++ src/main/java/com/maurofokker/java8/functionalinterfaces/ConsumerDemo.java
@@ -1,4 +1,67 @@
 package com.maurofokker.java8.functionalinterfaces;
 
+import com.maurofokker.java8.data.Student;
+import com.maurofokker.java8.data.StudentDataBase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class ConsumerDemo {
+
+    public static void main(String[] args) {
+
+        Consumer<String>  consumer = s -> System.out.println(s.toUpperCase());
+        consumer.accept("hello world");
+        // output: HELLO WORLD
+
+        List<String> names = Arrays.asList("John", "Doe");
+        //names.forEach(s -> System.out.println(s.toUpperCase()));
+        names.forEach(consumer);
+        // output: JOHN \n DOE
+
+        printStudentsName();
+        printStudentsNameAndActivities();
+        printNameAndActivitiesUsingCondition();
+
+    }
+
+    private static void printStudentsName() {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Printing students names");
+
+        Consumer<Student> consumer = student -> System.out.println(student);
+        List<Student> students = StudentDataBase.getAllStudents();
+        students.forEach(consumer);
+
+    }
+
+    private static void printStudentsNameAndActivities() {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Printing students names and activities");
+
+        Consumer<Student> studentsNameConsumer = student -> System.out.print(student.getName());
+        Consumer<Student> studentsActivitiesConsumer = student -> System.out.println(student.getActivities());
+
+        List<Student> students = StudentDataBase.getAllStudents();
+        // apply consumer chaining
+        students.forEach(studentsNameConsumer.andThen(studentsActivitiesConsumer));
+
+    }
+
+    public static void printNameAndActivitiesUsingCondition(){
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Printing students names and activities using conditions");
+
+        Consumer<Student> studentsNameConsumer = student -> System.out.print(student.getName());
+        Consumer<Student> studentsActivitiesConsumer = student -> System.out.println(student.getActivities());
+
+        List<Student> personList = StudentDataBase.getAllStudents();
+        personList.forEach((student) -> {
+            if( student.getGradeLevel() >= 3 && student.getGpa() > 3.9){
+                studentsNameConsumer.andThen(studentsActivitiesConsumer).accept(student);
+            }
+        });
+    }
+
 }
